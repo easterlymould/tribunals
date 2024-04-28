484b6435d3895048f8f02455ccf419e62f972aad
easterlymould <146983921+easterlymould@users.noreply.github.com>
2024-04-28 22:43:26 +0100

    New visualisation + new notebook for convictions
    - Second viz in js
    - New notebook to calculate conviction rate

--- linegraph.js
+++ linegraph.js
@@ -1,15 +1,18 @@
 // Using code from this: https://d3-graph-gallery.com/graph/line_several_group.html
 // and this: https://www.data-to-viz.com/caveat/spaghetti.html
 
-var containerWidth = document.getElementById('my_dataviz').getBoundingClientRect().width;
+
+var containerWidth = document.getElementById('my_dataviz1').getBoundingClientRect().width;
 
 var margin = { top: 10, right: 30, bottom: 30, left: 60 },
     width = containerWidth - margin.left - margin.right,
     height = (width / 800) * 500; 
 
+var axisFontSize = "14px"
+var tickValues = d3.range(2010, 2024, 2);
 
 // Append the svg object to the body of the page
-var svg = d3.select("#my_dataviz")
+var svg = d3.select("#my_dataviz1")
   .append("svg")
   .attr("viewBox", `0 0 ${width + margin.left + margin.right} ${height + margin.top + margin.bottom}`)
   .attr("preserveAspectRatio", "xMinYMin meet")
@@ -48,7 +51,6 @@ const categoryTexts = {
     try {
         const data = await d3.csv("yearly_charges_percentages_long_format_cleaned.csv");
 
-        // Convert 'year' and 'value' to numbers
         data.forEach(d => {
             d.year = +d.year;
             d.value = +d.value;
@@ -60,15 +62,21 @@ const categoryTexts = {
         var x = d3.scaleLinear()
             .domain(d3.extent(data, d => d.year))
             .range([0, width]);
-        svg.append("g")
+            svg.append("g")
             .attr("transform", "translate(0," + height + ")")
-            .call(d3.axisBottom(x).ticks(5).tickFormat(d3.format("d")));
+            .call(d3.axisBottom(x)
+                  .tickValues(tickValues)
+                  .tickFormat(d3.format("d")))
+            .selectAll("text")
+            .style("font-size", axisFontSize);
 
         var y = d3.scaleLinear()
             .domain([0, 100])
             .range([height, 0]);
         svg.append("g")
-            .call(d3.axisLeft(y));
+            .call(d3.axisLeft(y))
+            .selectAll("text") 
+            .style("font-size", axisFontSize);
 
         var yAxisFontSize = width / 40;
 
@@ -127,9 +135,9 @@ const categoryTexts = {
         function updateInfoDisplay(text) {
           infoText.selectAll("*").remove(); // Clear previous text
       
-          const rectHeight = 80; // Height of the rectangle
-          const x = 10; // x-coordinate where text starts
-          const maxWidth = 230; // Maximum width of text allowed before wrapping
+          const rectHeight = 80; 
+          const x = 10; 
+          const maxWidth = 230;
       
           // Start the first line of text
           var tspan = infoText.append("tspan")
@@ -144,10 +152,10 @@ const categoryTexts = {
               line.push(word);
               tspan.text(line.join(" "));
               if (tspan.node().getComputedTextLength() > maxWidth) {
-                  line.pop(); // Remove the word that causes the overflow
-                  tspan.text(line.join(" ")); // Set the text without the overflowing word
-                  line = [word]; // Start a new line with the overflowed word
-                  tspan = infoText.append("tspan") // Create new tspan for the new line
+                  line.pop();
+                  tspan.text(line.join(" ")); 
+                  line = [word]; 
+                  tspan = infoText.append("tspan")
                       .attr("x", x)
                       .attr("dy", "1.2em")
                       .text(word);
@@ -174,3 +182,72 @@ const categoryTexts = {
         console.error('Error with CSV:', error);
     }
 })();
+
+function setupBasicLineGraph() {
+    console.log("Setting up the basic line graph...");
+
+    // Use the same container width calculation for consistent responsiveness
+    var containerWidth = document.getElementById('my_dataviz2').getBoundingClientRect().width;
+    var margin = { top: 10, right: 30, bottom: 30, left: 60 },
+        width = containerWidth - margin.left - margin.right,
+        height = (width / 800) * 500;
+
+    // Create the SVG container with the same viewBox and preserveAspectRatio
+    var svg = d3.select("#my_dataviz2")
+        .append("svg")
+        .attr("viewBox", `0 0 ${width + margin.left + margin.right} ${height + margin.top + margin.bottom}`)
+        .attr("preserveAspectRatio", "xMinYMin meet")
+        .append("g")
+        .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
+
+    // Scales and axes setup with the same domains and styles
+    var x = d3.scaleLinear()
+            .domain([2010, 2023])
+            .range([0, width]);
+    var y = d3.scaleLinear()
+            .domain([0, 100])
+            .range([height, 0]);
+    var color = d3.scaleOrdinal(d3.schemeCategory10);
+
+    d3.csv("long_format_conviction_rates.csv").then(function(data) {
+        console.log("Data loaded successfully:", data);
+
+        data.forEach(function(d) {
+            d.year = +d.year;
+            d.value = +d.value;
+        });
+
+        var sumstat = d3.group(data, d => d.category);
+
+        svg.append("g")
+   .attr("transform", "translate(0," + height + ")")
+   .call(d3.axisBottom(x)
+         .tickValues(tickValues)
+         .tickFormat(d3.format("d")))
+   .selectAll("text")
+   .style("font-size", axisFontSize);
+
+    // Set up the Y Axis with larger font size
+    svg.append("g")
+        .call(d3.axisLeft(y))
+        .selectAll("text")
+        .style("font-size", axisFontSize);
+
+        sumstat.forEach(function(values, key) {
+            svg.append("path")
+                .datum(values)
+                .attr("class", "line")
+                .attr("fill", "none")
+                .attr("stroke", color(key))
+                .attr("stroke-width", 3)
+                .attr("d", d3.line()
+                    .x(d => x(d.year))
+                    .y(d => y(d.value))
+                );
+        });
+    }).catch(function(error) {
+        console.error('Error loading the CSV:', error);
+    });
+}
+
+setupBasicLineGraph();
